b819b5e355d4a038ea249acdad8ba428d322e573
kyosyun <kyosyunn@yahoo.co.jp>
2019-09-09 02:20:48 +0900

    add introduction

--- Introduction/Nullable types/src/Task.kt
+++ Introduction/Nullable types/src/Task.kt
@@ -1,7 +1,9 @@
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ){
-    TODO()
+    val email= client?.personalInfo?.email ?: return
+    val message = message ?: return
+    mailer.sendMessage(email, message)
 }
 
 class Client (val personalInfo: PersonalInfo?)
--- Introduction/Object expressions/src/Task.kt
+++ Introduction/Object expressions/src/Task.kt
@@ -2,6 +2,10 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object: Comparator<Int>{
+        override fun compare(x: Int, y: Int): Int {
+            return y - x
+        }
+    })
     return arrayList
 }
