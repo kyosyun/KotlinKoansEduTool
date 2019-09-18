b06daab4c33813bc715dc85b7e4cad86182e1590
許 駿 <kyosyunn@yahoo.co.jp>
2019-09-18 19:28:18 +0900

    add lazy property

--- Properties/Lazy property/src/Task.kt
+++ Properties/Lazy property/src/Task.kt
@@ -1,8 +1,12 @@
 class LazyProperty(val initializer: () -> Int) {
 
+    var value: Int? = null
+
     val lazy: Int
         get() {
+            if (value == null) {
+                value = initializer()
+            }
+            return value!!
         }
-
-
 }
